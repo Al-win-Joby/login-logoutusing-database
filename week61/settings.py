14ef6721a870fd0b11620f39dfe6360d3c36acc8
Al-win-Joby <104632249+Al-win-Joby@users.noreply.github.com>
2022-12-27 13:46:56 +0530

    Update settings.py

--- week61/settings.py
+++ week61/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db4login',
         'USER':  'postgres',
-        'PASSWORD':'mibyjoby',
+        'PASSWORD':'',
         'HOST': 'localhost'
     }
 }
